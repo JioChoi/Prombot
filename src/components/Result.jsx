dbf5f8f182a42e400adc4cce013459392b077299
jiochoi <jiochoi7@gmail.com>
2025-01-20 13:50:09 -0500

    Trying out with fixed

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -10,8 +10,8 @@ function Result() {
 
     return (
         <div className={`
-        bg-zinc-900 w-screen h-[calc(100vh-133px)] flex items-center justify-center mb-[39px] shadow-md
-        lg:w-[calc(100%-450px)] lg:h-screen lg:mb-0 ${animation}
+        bg-zinc-900 w-screen h-[calc(100%-133px)] fixed top-0 left-0 flex items-center justify-center mb-[39px] shadow-md
+        lg:w-[calc(100%-450px)] lg:h-screen lg:mb-0 lg:static ${animation}
         `}>
         <img className="max-w-[85%] max-h-[85%]" src={data.current_image}></img>
         </div>
