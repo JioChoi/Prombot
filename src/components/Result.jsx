bb63c15e2c962eab73bd525d86b2845416a685bd
jiochoi <jiochoi7@gmail.com>
2025-01-20 13:25:41 -0500

    Fix broken mobile ui

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -11,7 +11,7 @@ function Result() {
     return (
         <div className={`
         bg-zinc-900 w-screen h-[calc(100vh-133px)] flex items-center justify-center mb-[39px] shadow-md
-        lg:w-[calc(100vw-450px)] lg:h-screen lg:mb-0 ${animation}
+        lg:w-[calc(100%-450px)] lg:h-screen lg:mb-0 ${animation}
         `}>
         <img className="max-w-[85%] max-h-[85%]" src={data.current_image}></img>
         </div>
