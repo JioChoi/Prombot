0e30da41ada27c64fba9a2d7a13f5e2aca7c350d
jiochoi <jiochoi7@gmail.com>
2025-01-25 19:02:09 -0500

    Change to google

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState, useRef, useMemo, createRef } from "react";
 import { datasets } from "@/lib/NAI";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Label } from "@/components/ui/label";
 
 import Icon from "@/components/ui/icon";
 
@@ -9,6 +11,8 @@ export default function Characters() {
     const [elements, dispatchElements] = useState([]);
     const [scrollHeight, dispatchScrollHeight] = useState(0);
 
+    const faceFocus = useRef(true);
+
     const [elementsHeight, dispatchElementsHeight] = useState(0);
 
     const prvScrollY = useRef(0);
@@ -16,11 +20,10 @@ export default function Characters() {
 
     const scroll = useRef(null);
 
-    const folders = ["0", "10k", "20k"];
     const buffer = 100;
 
     function updateElementsHeight() {
-        let width = window.innerWidth;
+        let width = window.innerWidth - 10;
         let col = 2;
         if (width > 1024) {
             col = 4;
@@ -30,19 +33,20 @@ export default function Characters() {
         dispatchElementsHeight((size + 16) * (Math.ceil(elements.length / col))); 
     }
     function updateScrollHeight() {
-        let width = window.innerWidth;
+        let width = window.innerWidth - 10;
         let col = 2;
         if (width > 1024) {
             col = 4;
         }
 
         let size = (Math.min(1200, width) - 16 * (col + 1)) / col;
+
         dispatchScrollHeight((size + 16) * (Math.ceil(data.length / col)));
     }
     function updateToScroll(clear = false) {
         let y = scroll.current.scrollTop;
 
-        let width = window.innerWidth;
+        let width = window.innerWidth - 10;
         let col = 2;
         if (width > 1024) {
             col = 4;
@@ -64,6 +68,15 @@ export default function Characters() {
         }
         let bf = Math.min(end, data.length - start);
 
+        if (bf <= 0) {
+            return;
+        }
+
+        let imgStyle = "object-contain"
+        if (faceFocus.current) {
+            imgStyle = "object-cover object-top";
+        }
+
         let temp = [...Array(bf).keys()].map((i) => {
             let temp = data[start + i];
             if (temp == undefined) {
@@ -75,7 +88,7 @@ export default function Characters() {
                 <div className={`relative rounded-lg aspect-square bg-zinc-800 overflow-hidden hover:cursor-pointer hover:brightness-90
                 `}
                     onClick={() => {
-                        if (copyright == -1) {
+                        if (temp.type == "copyright") {
                             prvScrollY.current = scroll.current.scrollTop;
                             dispatchCopyright(temp.id);
                         }
@@ -85,8 +98,12 @@ export default function Characters() {
                         }
                     }}
                 >
-                    <img className="w-full h-full object-contain pointer-events-none" loading="lazy"
-                        src={`https://huggingface.co/Jio7/NAI-Prompt-Randomizer/resolve/main/characters/${folders[folder]}/${temp.img}.webp`}
+                    {
+                        temp.type == "character" ?
+                        <div className="absolute top-1 left-1 w-[32px] h-[32px] bg-black flex align-middle justify-center rounded-full"><Icon name="user-3-fill" className="text-2xl text-white"/></div> : ""
+                    }
+                    <img className={"w-full h-full pointer-events-none " + imgStyle} loading="lazy"
+                        src={`https://huggingface.co/Jio7/NAI-Prompt-Randomizer/resolve/main/characters/${folder}/${temp.img}.webp`}
                     >
                     </img>
                     <div
@@ -124,33 +141,35 @@ export default function Characters() {
             let result = datasets.characters_copyright.map((temp, index) => {
                 return {
                     "name": temp[0],
-                    "img": datasets.characters_characters[temp[2][0]].img,
-                    "id": index
+                    "img": datasets.characters_characters[temp[2][0]].id,
+                    "id": index,
+                    "type": "copyright"
                 }
             }).filter((temp) => temp.name.toLowerCase().includes(lowSearch));
 
-            // if (lowSearch != "") {
-            //     result = result.concat(datasets.characters_characters.map((temp, index) => {
-            //         return {
-            //             "name": temp.name,
-            //             "img": temp.img,
-            //             "id": index,
-            //             "tags": temp.tags
-            //         }
-            //     }).filter((temp) => {
-            //         let name = temp.name.toLowerCase().split(' (')[0];
+            if (lowSearch != "") {
+                result = result.concat(datasets.characters_characters.map((temp, index) => {
+                    return {
+                        "name": temp.name,
+                        "img": temp.id,
+                        "id": index,
+                        "tags": temp.tags,
+                        "type": "character"
+                    }
+                }).filter((temp) => {
+                    let name = temp.name.toLowerCase().split(' (')[0];
 
-            //         if (name.includes(lowSearch)) {
-            //             return true;
-            //         }
+                    if (name.includes(lowSearch)) {
+                        return true;
+                    }
                     
-            //         if (arrSearch.every((search) => temp.tags.some((tag) => tag[0].substring(0, search.length) == search))) {
-            //             return true;
-            //         }
+                    if (arrSearch.every((search) => temp.tags.some((tag) => tag[0].substring(0, search.length) == search))) {
+                        return true;
+                    }
 
-            //         return false;
-            //     }));
-            // }
+                    return false;
+                }));
+            }
             
             return result;
         }
@@ -159,7 +178,8 @@ export default function Characters() {
             return temp.filter((temp) => datasets.characters_characters[temp] != undefined).map((temp, index) => {
                 return {
                     "name": datasets.characters_characters[temp].name,
-                    "img": datasets.characters_characters[temp].img
+                    "img": datasets.characters_characters[temp].id,
+                    "type": "character"
                 }
             });
         }
@@ -189,7 +209,8 @@ export default function Characters() {
                 e.preventDefault();
                 e.target[0].blur();
             }}>
-                <div className="fixed w-full h-12 z-40 top-[32px] left-0 bg-zinc-800 flex items-center hover:cursor-pointer hover:brightness-90"
+                <div className="fixed w-full h-20 z-40 top-[32px] left-0 bg-zinc-800">
+                <div className="h-12 flex items-center hover:cursor-pointer hover:brightness-90 bg-zinc-800"
                     onClick={() => {
                         if (copyright != -1) {
                             dispatchCopyright(-1);
@@ -204,7 +225,7 @@ export default function Characters() {
                     <Icon name="left-line" className="text-3xl px-3"/> :
                     <input
                         className="outline-none w-full h-full bg-zinc-800 text-white px-4 text-lg"
-                        placeholder="Search for the copyright"
+                        placeholder="Search for the copyright, character, or tags"
                         onChange={(e) => {
                             dispatchSearch(e.target.value);
                             scroll.current.scrollTop = 0; 
@@ -213,9 +234,24 @@ export default function Characters() {
                     ></input>
                     }
                 </div>
+                <div className="mt-2 ml-4">
+                    <div className="flex items-center space-x-1">
+                        <Checkbox id="faceFocus" 
+                            checked={ faceFocus.current }
+                            onCheckedChange={(checked) => {
+                                faceFocus.current = checked;
+                                updateToScroll(true);
+                            }}
+                        />
+                        <Label htmlFor="faceFocus" className="m-0 hover:cursor-pointer">Focus on Face</Label>
+                    </div>
+                </div>
+                    
+
+                </div>
             </form>
 
-            <div ref={scroll} className="h-[calc(100%-80px)] w-full overflow-y-scroll mt-[48px]"
+            <div ref={scroll} className="h-[calc(100%-80px-32px)] w-full overflow-y-scroll mt-[80px]"
                 onScroll={(e) => {
                     if (elementsHeight - 100 < e.target.scrollTop + e.target.clientHeight) {
                         updateToScroll();
