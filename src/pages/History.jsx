346755f02de5ff6666e101a5c785e1453bd65e86
jiochoi <jiochoi7@gmail.com>
2025-02-16 22:44:54 -0500

    Move selected

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -57,6 +57,13 @@ export default function History() {
         }
     }, [selected, data.history]);
 
+    useEffect(() => {
+        if (data.history.length == 0 || selected == 0) {
+            return;
+        }
+        setSelected(selected - 1);
+    }, [data.history]);
+
     let animation = data.generating ? "generating" : "";
 
     return (
