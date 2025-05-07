a52f2d0c3f475538328789c84195660c4f3975df
jiochoi <jiochoi7@gmail.com>
2025-05-07 13:29:10 -0400

    Curated

--- src/modules/Dev.jsx
+++ src/modules/Dev.jsx
@@ -46,7 +46,7 @@ export default function Dev() {
                     { value: 'nai-diffusion-3', label: 'NAI Diffusion V3' },
                     { value: 'nai-diffusion-4-curated-preview', label: 'NAI Diffusion V4 Preview' },
                     { value: 'nai-diffusion-4-full', label: 'NAI Diffusion V4' },
-                    { value: 'nai-diffusion-4.5-curated-preview', label: 'NAI Diffusion V4.5 Preview' },
+                    { value: 'nai-diffusion-4.5-curated', label: 'NAI Diffusion V4.5 Curated' },
                     { value: 'nai-diffusion-4.5', label: 'NAI Diffusion V4.5' },
                 ]} />
                 
